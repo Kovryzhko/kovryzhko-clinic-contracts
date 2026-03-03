7cc4bf5069a305e60c5522427467bc7ee66983ba
Lev <lev.kovryzhko@bk.ru>
2026-03-03 14:31:06 +0300

    fix: imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './proto/paths'
 export * from './events/auth/otp.request.interface'
+export * from './events/account/phone-changed.interface'
+export * from './events/account/email-changed.interface'
